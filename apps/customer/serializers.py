eea6b9b4f2790e7419d38408f197a941053c6ba2
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2023-11-07 15:00:22 +0500

    fix some changes

--- apps/customer/serializers.py
+++ apps/customer/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.pagination import PageNumberPagination
-
+from apps.product.models import ProductItem, Good, Phone, Ticket
 from .models import Favorite, Location, News, Profile, ViewedNews
 
 
@@ -35,6 +35,37 @@ class ViewedNewsSerializer(serializers.ModelSerializer):
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = "__all__"
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = "__all__"
+
+class PhoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phone
+        fields = "__all__"
+
+class GoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Good
+        fields = "__all__"
+
+class ProductItemForFavouriteSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(read_only=True)
+    phones = PhoneSerializer(read_only=True)
+    goods = GoodSerializer(read_only=True)
+
+    class Meta:
+        model = ProductItem
+        fields = "__all__"
+
+class FavoriteListSerializer(serializers.ModelSerializer):
+    product = ProductItemForFavouriteSerializer()
+
     class Meta:
         model = Favorite
         fields = "__all__"
