3869f119537f15b295ac2e0fefc2a542111533c8
karimoff <doniyorkarimoff96@gmail.com>
2023-11-11 22:39:44 +0500

    template fix

--- apps/product/dashboard/forms.py
+++ apps/product/dashboard/forms.py
@@ -141,9 +141,6 @@ class TicketProductItemForm(forms.ModelForm):
         widgets = {
             'category': forms.Select(attrs={'class': 'form-control'}),
             'event_name': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'event_date': forms.DateInput(
-            #     attrs={'type': 'date', 'placeholder': 'yyyy-mm-dd (DOB)', 'class': 'form-control'}
-            # )
         }
     
     category = forms.ModelChoiceField(
@@ -207,6 +204,19 @@ class TicketProductItemForm(forms.ModelForm):
                 image.save()
         return ticket
 
+class GoodCategoryCreateForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'image', 'desc', 'stock', 'bonus', 'active']
+        widgets = {
+            # 'main_type': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'desc': forms.Textarea(attrs={'class': 'form-control'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+            'bonus': forms.NumberInput(attrs={'class': 'form-control'}),
+            'active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
 
 class GoodProductItemForm(forms.ModelForm):
     class Meta:
