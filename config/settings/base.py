95283bca23a4b5087a87fbde22c0cc1edc7770ac
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2023-11-20 11:51:19 +0500

    media fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -165,7 +165,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "../", "media/")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "../", "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles/")
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
