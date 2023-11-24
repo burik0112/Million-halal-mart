2d71d8bcead6a3828c931e7bf1f280bfe9bc0a00
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2023-11-24 20:07:23 +0500

    all product search

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,26 +1,38 @@
 from django.urls import path
-from .views import PopularTicketsAPIView, PopularPhonesAPIView, PopularGoodAPIView, TicketsOnSaleListView, PhonesOnSaleListView, GoodsOnSaleListView
-from . import views
+from .views import (
+    NewPhonesListView,
+    PhoneListAPIView,
+    NewTicketsListView,
+    TicketListAPIView,
+    SubCategoryListAPIView,
+    CategoryListAPIView,
+    PopularTicketsAPIView,
+    PopularPhonesAPIView,
+    PopularGoodAPIView,
+    TicketsOnSaleListView,
+    PhonesOnSaleListView,
+    GoodsOnSaleListView,
+    GoodListAPIView,
+    NewGoodsListView,
+    ImageListAPIView,
+    MultiProductSearchView,
+)
 
 urlpatterns = [
-    path("categories/list/", views.CategoryListAPIView.as_view()),
-    path("subcats/list/", views.SubCategoryListAPIView.as_view()),
-
-    path("tickets/list/", views.TicketListAPIView.as_view()),
-    path("new-tickets/list/", views.NewTicketsListView.as_view()),
+    path("categories/list/", CategoryListAPIView.as_view()),
+    path("subcats/list/", SubCategoryListAPIView.as_view()),
+    path("tickets/list/", TicketListAPIView.as_view()),
+    path("new-tickets/list/", NewTicketsListView.as_view()),
     path("popular-tickets/list/", PopularTicketsAPIView.as_view()),
     path("sale-tickets/list/", TicketsOnSaleListView.as_view()),
-
-    path("phones/list/", views.PhoneListAPIView.as_view()),
-    path("new-phones/list/", views.NewPhonesListView.as_view()),
+    path("phones/list/", PhoneListAPIView.as_view()),
+    path("new-phones/list/", NewPhonesListView.as_view()),
     path("popular-phones/list/", PopularPhonesAPIView.as_view()),
     path("sale-phones/list/", PhonesOnSaleListView.as_view()),
-
-    path("goods/list/", views.GoodListAPIView.as_view()),
-    path("new-goods/list/", views.NewGoodsListView.as_view()),
+    path("goods/list/", GoodListAPIView.as_view()),
+    path("new-goods/list/", NewGoodsListView.as_view()),
     path("popular-goods/list/", PopularGoodAPIView.as_view()),
     path("sale-goods/list/", GoodsOnSaleListView.as_view()),
-
-
-    path("images/list/", views.ImageListAPIView.as_view()),
+    path("images/list/", ImageListAPIView.as_view()),
+    path("product-search/", MultiProductSearchView.as_view()),
 ]
