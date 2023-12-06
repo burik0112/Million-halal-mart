e687e11125215b9cdf6a7d0d69589d3c3d0f028b
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2023-12-06 10:05:04 +0500

    deploy to server

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,17 +110,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME"),
-#         "USER": "username",
-#         "PASSWORD": "strongpassword",
-#         "HOST": "pgbouncer",
-#         "PORT": '6432',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
