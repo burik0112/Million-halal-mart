297994c8bb632eafe547a10694277000e6d8e4e5
Doniyorbek Karimov <90027802+karimoff96@users.noreply.github.com>
2024-01-11 15:44:11 +0500

    Update admin.py

--- apps/customer/admin.py
+++ apps/customer/admin.py
@@ -5,7 +5,9 @@ from .models import *
 # Register your models here.
 class NewsAdmin(admin.ModelAdmin):
     list_display=['start_date','title', 'end_date', 'description']
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=['phone_number','full_name', 'created']
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Location)
 admin.site.register(News, NewsAdmin)
 admin.site.register(ViewedNews)
