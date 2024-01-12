901eca21fa518d212f67613db85fd9c9a5e49aea
Doniyorbek Karimov <90027802+karimoff96@users.noreply.github.com>
2024-01-12 16:51:22 +0500

    Update users.py

--- apps/dashboard/users.py
+++ apps/dashboard/users.py
@@ -14,7 +14,7 @@ class UserListView(ListView):
     context_object_name = "users"
 
     def get_queryset(self):
-        return Profile.objects.all()
+        return Profile.objects.all().order_by('pk')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
