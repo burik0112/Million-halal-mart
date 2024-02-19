4f539a41d85dfa815c65cb2d188ba15bbee37d80
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2024-02-19 23:48:24 +0500

    is favorite = True

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -122,15 +122,16 @@ class FavoriteCreateAPIView(CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user.profile)
+        serializer.save(user=self.request.user.profile,product__is_favorite=True)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
+        
         try:
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
+            
             return Response(
                 serializer.data, status=status.HTTP_201_CREATED, headers=headers
             )
