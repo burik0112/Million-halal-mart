7ba068e9fac8d4a56cc3cfb7bd1d178c480913c6
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2024-05-09 22:00:29 +0500

    media

--- init.sql
+++ init.sql
@@ -1,2 +1,3 @@
-CREATE USER username WITH PASSWORD 'strongpassword';
+-- CREATE USER username WITH PASSWORD 'strongpassword';
+CREATE USER bouncer WITH PASSWORD '1bouncerpassversion!';
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO username;
