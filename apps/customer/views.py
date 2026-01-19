36cb44135b0fa9addcc820fa5fef534b410e848b
burik <burhonabdullaev727@gmail.com>
2026-01-19 18:52:07 +0500

    jwt

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -14,6 +14,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, status, permissions
 from django.db import IntegrityError, transaction
+from rest_framework_simplejwt.tokens import RefreshToken
 from twilio.rest import Client
 from django.conf import settings
 from rest_framework.response import Response
